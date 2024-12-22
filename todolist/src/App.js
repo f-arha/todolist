873d68e6c74b86099df7e4ff79355688365a5ebc
f-arha <190675045+f-arha@users.noreply.github.com>
2024-12-22 21:46:33 +0530

    Add marking and display done works

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -16,15 +16,24 @@ function App() {
         </div>
         <div className="input">
           <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊 Add item..." />
-          <i onClick={()=>setToDos([...toDos,toDo])} className="fas fa-plus"></i>
+          <i onClick={()=>setToDos([...toDos,{id:Date.now(), text :toDo, status: false}])} className="fas fa-plus"></i>
         </div>
         <div className="todos">
-        {  toDos.map((value)=>{
-          
+        {  toDos.map((obj)=>{
+
          return (  <div className="todo">
             <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>{value}</p>
+              <input onChange={(e)=>{
+                console.log(e.target.checked)
+                console.log(obj)
+                setToDos(toDos.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj.status=e.target.checked
+                  }
+                  return obj2
+                }))
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
             </div>
             <div className="right">
               <i className="fas fa-times"></i>
@@ -32,6 +41,14 @@ function App() {
           </div> )
             })
             }
+            {toDos.map((obj)=>{
+              if(obj.status){
+                return(
+                  <h1>{obj.text}</h1>
+                )
+              }
+              return null
+            })}
         </div>
       </div>
     );
